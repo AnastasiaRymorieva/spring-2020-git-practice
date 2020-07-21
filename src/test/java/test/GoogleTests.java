c162cac52b25ed1f7a8de73a738af91198c79f4f
AnastasiaRymorieva <nastyarymorieva@gmail.com>
2020-07-21 16:51:32 -0400

    Google Title verification test

---
+++ src/test/java/test/GoogleTests.java
@@ -0,0 +1,34 @@
+package test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class GoogleTests {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("http://google.com");
+    }
+
+    @Test
+    public void test(){
+        Assert.assertEquals(driver.getTitle(),"Google");
+    }
+    @AfterMethod
+    public void tearDown()
+    {
+        driver.quit();
+    }
+
+
+
+}
